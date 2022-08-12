04f62305a45429b75c111249a3be84f257dd30e5
Jiro Ikeda <stapfgard@gmail.com>
2022-08-12 13:50:54 +0900

    Bran new.

--- picture-frame.el
+++ picture-frame.el
@@ -2,30 +2,44 @@
 (require 'request)
 
 (defconst picture-frame-buffer " *picture-frame-buffer*")
+(defcustom picture-frame-image-conversion 'enabled "image conversion")
+(defcustom picture-frame-image-pointer 'arrow "image pointer")
+(defcustom picture-frame-image-relief 0 "image relief")
+(defcustom picture-frame-image-rotation 0 "image rotation")
+(defcustom picture-frame-image-size (lambda (frame) '(960 540)) "image size")
+(defcustom picture-frame-timer-interval 1 "timer interval")
+(defcustom picture-frame-url (lambda () (message "No picture-frame-url.")) "url")
 (defvar picture-frame-data nil)
-(defvar picture-frame-frame-height (lambda () 450))
-(defvar picture-frame-frame-width (lambda () 590))
+(defvar picture-frame-frame nil)
 (defvar picture-frame-poshandler 'posframe-poshandler-frame-bottom-right-corner)
 (defvar picture-frame-timer nil)
-(defvar picture-frame-timer-interval 1)
-(defvar picture-frame-url (lambda () (message "No picture-frame-url.")))
 (defvar picture-frame-url-got nil)
 
 (defun picture-frame-load ()
 	(when (posframe-workable-p)
-		(with-current-buffer (get-buffer-create picture-frame-buffer)
-			(erase-buffer)
-			(insert-image
-				(create-image (encode-coding-string picture-frame-data 'utf-8) nil t
-					:height (funcall picture-frame-frame-height)
-					:pointer 'arrow
-					:width (funcall picture-frame-frame-width)
+		(let*
+			(
+				(size (funcall picture-frame-image-size picture-frame-frame))
+				(width (nth 0 size))
+				(height (nth 1 size))
+			)
+			(with-current-buffer (get-buffer-create picture-frame-buffer)
+				(erase-buffer)
+				(insert-image
+					(create-image (encode-coding-string picture-frame-data 'utf-8) nil t
+						:conversion picture-frame-image-conversion
+						:height height
+						:pointer 'picture-frame-image-pointer
+						:relief picture-frame-image-relief
+						:rotation picture-frame-image-rotation
+						:width width
+					)
 				)
 			)
-		)
-		(posframe-show picture-frame-buffer
-			:keep-ratio t
-			:poshandler picture-frame-poshandler
+			(posframe-show picture-frame-buffer
+				:keep-ratio t
+				:poshandler picture-frame-poshandler
+			)
 		)
 	)
 )
@@ -46,7 +60,7 @@
 	(setq picture-frame-url-got nil)
 	(when picture-frame-timer (cancel-timer picture-frame-timer))
 	(when (posframe-workable-p) (posframe-delete picture-frame-buffer))
-	(message "picture-frame stopped.")
+	(message "picture-frame: stopped.")
 )
 
 (defun picture-frame-update (url)
@@ -70,7 +84,8 @@
 )
 
 (add-hook 'window-size-change-functions (lambda (frame)
-	(when (not (null picture-frame-data)) (picture-frame-load))
+	(setq picture-frame-frame frame)
+	(when picture-frame-mode (picture-frame-load))
 ))
 
 (define-minor-mode picture-frame-mode
